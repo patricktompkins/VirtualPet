53b47be3d2fb454c333d804f85a5e02e020ae20f
patricktompkins <patrick.c.tompkins@gmail.com>
2018-02-03 18:02:16 -0500

    first commit

---
+++ VirtualPet/MyPet.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    public class MyPet
+    {
+        //fields 
+        private string petType;
+        private int age;
+        private bool likesToTalk;
+
+        //properties
+        public string PetType
+        {
+            get { return this.petType; }
+            set { this.petType = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age;  }
+            set { this.age = value; }
+        }
+
+        public bool LikesToTalk
+        {
+            get { return this.likesToTalk; }
+            set { this.likesToTalk = value; }
+        }
+
+        //Constructors
+        public MyPet()
+        {
+            this.petType = "Mr. Ed The Talking Horse";
+            this.age = 3;
+            this.likesToTalk = true;
+        }  // Do we need overriding constructors
+
+        //Methods
+
+        public void TalkToThePet()
+        {
+           if (likesToTalk)
+            {
+                Console.WriteLine("Yes I like to talk.");
+            }
+            else
+            {
+                Console.WriteLine("No I don't like to talk");
+            }
+           
+        }
+
+        public void ProvideInformation()
+        {
+            Console.WriteLine("I am a {0} years old {1} that {2} to talk.", age, petType, likesToTalk);
+        }
+
+        public void AskQuestion(int question)
+        {
+            switch (question)
+            {
+                case 1:
+                    Console.WriteLine(age);
+                    break;
+
+                case 2:
+                    Console.WriteLine(likesToTalk);
+                    break;
+                case 3:
+                    Console.WriteLine(petType);
+                    break;
+
+                default:
+                    break;
+
+            }
+        }
+        private void Tick()
+        {
+            //Need to fill in Tick Method
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ VirtualPet/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyPet Ed = new MyPet();
+            Ed.ProvideInformation();
+            Console.WriteLine("Do you like to talk? {0}", Ed.LikesToTalk);
+            bool isEndProgram = false;
+
+            do
+            {
+                //tick method
+                Ed.Tick();
+                //place menu here
+
+                //Enter the display output
+
+
+            } while (isEndProgram);
+
+           
+        }
+    }
+}
