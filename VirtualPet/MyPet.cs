e0a5bf8f08fa4450f033a0bfd9c1e3438d8f5c11
patricktompkins <patrick.c.tompkins@gmail.com>
2018-02-04 18:15:54 -0500

    final Commit

--- VirtualPet/MyPet.cs
+++ VirtualPet/MyPet.cs
@@ -12,6 +12,8 @@ namespace VirtualPet
         private string petType;
         private int age;
         private bool likesToTalk;
+        private int hungaryLevel;
+        private string collegeTeam;
 
         //properties
         public string PetType
@@ -32,12 +34,27 @@ namespace VirtualPet
             set { this.likesToTalk = value; }
         }
 
+
+        public int HungaryLevel
+        {
+            get { return this.hungaryLevel; }
+            set { this.hungaryLevel = value; }
+        }
+
+        public string NccaTeam
+        {
+            get { return this.collegeTeam; }
+            set { this.collegeTeam = value; }
+        }
+
         //Constructors
         public MyPet()
         {
-            this.petType = "Mr. Ed The Talking Horse";
-            this.age = 3;
+            this.petType = "Horse";
+            this.age = 0;
             this.likesToTalk = true;
+            this.hungaryLevel = 12;
+            this.collegeTeam = "Not Selected";
         }  // Do we need overriding constructors
 
         //Methods
@@ -78,12 +95,81 @@ namespace VirtualPet
                 default:
                     break;
 
+
             }
         }
-        private void Tick()
-        {
-            //Need to fill in Tick Method
 
+        public void SelectCollegeTeam()
+        {
+            switch (age)
+            {
+                case 1:
+                    collegeTeam = "The 2017 NCAA Footaball National Champions Alabama Crimson Tide";
+                    Console.WriteLine("The 2017 NCAA Footaball National Champions Alabama Crimson Tide");
+                    break;
+
+                case 2:
+                    collegeTeam = "Ohio State Buckeyes";
+                    Console.WriteLine("Ohio State Buckeyes");
+                    break;
+                case 3:
+                    collegeTeam = "Florida Gators";
+                    Console.WriteLine("Florida Gators");
+                    break;
+
+                case 4:
+                    collegeTeam = "Norte Dame Fighting Irish";
+                    Console.WriteLine("Norte Dame Fighting Irish");
+                    break;
+
+                default:
+                    collegeTeam = "The 2017 NCAA Footaball National Champions Alabama Crimson Tide";
+                    Console.WriteLine("The 2017 NCAA Footaball National Champions Alabama Crimson Tide");
+                    break;
+
+
+            }
+
+            
+
+        }
+
+
+        //TICK METHOD
+        public void Tick()
+        {
+            string level;
+            string speak;
+            
+
+            HungaryLevel = HungaryLevel + 1;
+            
+            
+            Console.WriteLine("I am Mr. Ed the talking {0}.", PetType );
+            Console.WriteLine("I am {0} years old.", Age);
+            if (LikesToTalk )
+            {
+             speak = "like to talk";
+            }
+            else
+            {
+
+            speak = "do not like to talk";
+            }
+            Console.WriteLine("I {0} all the time.", speak);
+            
+            if (HungaryLevel < 10)
+            {
+                level = "I am not hungary.";
+            }
+            else 
+            {
+                level = "I am hungary.";
+            }
+            Console.WriteLine(level);
+            Console.WriteLine("The team is {0} ", collegeTeam);
+
+            Console.WriteLine();
         }
 
 
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -11,20 +11,94 @@ namespace VirtualPet
         static void Main(string[] args)
         {
             MyPet Ed = new MyPet();
-            Ed.ProvideInformation();
-            Console.WriteLine("Do you like to talk? {0}", Ed.LikesToTalk);
+            //Ed.ProvideInformation();
+            //Console.WriteLine("Do you like to talk? {0}", Ed.LikesToTalk);
             bool isEndProgram = false;
+            int endProgram = 0;
 
             do
             {
                 //tick method
                 Ed.Tick();
                 //place menu here
+                Console.WriteLine("Please select how you want to interact with your virtual pet?");
+                Console.WriteLine("select 1, To change age.");
+                Console.WriteLine("select 2, To change the type of virtural pet.");
+                Console.WriteLine("select 3, To change talking status?");
+                Console.WriteLine("select 4, To feed your pet.");
+                Console.WriteLine("select 5, A college sports team to assign your virtual pet to be their new mascot");
+                Console.WriteLine("select 6, To Exit");
+                Console.WriteLine();
+                Console.Write("> ");
+                endProgram = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                
 
-                //Enter the display output
 
 
-            } while (isEndProgram);
+                //Enter the display output 
+                Ed.LikesToTalk = true;
+                if (endProgram == 1)
+
+                {
+                    Console.WriteLine("You increased age."); 
+                    Ed.Age = Ed.Age + 1;
+                    
+
+                }
+
+                else if (endProgram == 2)
+                {
+                    Console.WriteLine("Please enter the type of Virtual Pet.");
+                    string input = Console.ReadLine();
+                    Ed.PetType = input;
+                    Console.WriteLine();
+                    Console.WriteLine("You just set a name for your virtual pet type.");
+                }
+
+                else if (endProgram == 3)
+                {
+                    Console.WriteLine("Daniel, I don't like to talk because you will not feed me.");
+                    Console.WriteLine("Daniel Maybe if you feed me I would like to talk you!!!!!");
+                    Ed.LikesToTalk = false;
+                }
+
+                
+                
+                else if (endProgram == 4)
+                  
+                {
+                    Console.WriteLine("You feed your {0} virutal", Ed.PetType);
+                    Ed.HungaryLevel = Ed.HungaryLevel - 3;
+
+                }
+                
+                else if( endProgram  == 5)
+                {
+                    Console.WriteLine("You increased age, picked another team.");
+                    Ed.Age = Ed.Age + 1;
+                    Ed.SelectCollegeTeam();
+                    Console.WriteLine();
+                    if (Ed.Age > 5)
+                    {
+                        Ed.Age = 0;
+                    }
+
+                }
+
+                else if (endProgram == 6)
+                {
+                    Console.WriteLine("You are ending the program");
+                    isEndProgram = true;
+                }
+
+                else
+                {
+                    
+                }
+
+                Console.WriteLine("\n\n");
+            } while (!isEndProgram);
 
            
         }
